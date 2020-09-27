debc84c588467a19c2915f2fc7333beb7528ba6f
Alex Merced <willcodeforfood@alexmerced.com>
2020-09-27 15:31:59 -0400

    env

--- envfunc.js
+++ envfunc.js
@@ -3,7 +3,7 @@ const yenv = require("yenv");
 
 module.exports = (envfile = "env.yaml", path = "./env.yaml") => {
   if (file.existsSync(path)) {
-    const env = yenv(envfile, { env: process.env.NODE_ENV });
+    const env = yenv(envfile, { env: process.env.NODE_ENV || "development" });
     process.env = { ...process.env, ...env };
   }
 };
