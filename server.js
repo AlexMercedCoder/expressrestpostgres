9c4ac42fdedda210cf24851b78fa14c636ab69be
Alex Merced <alex@alexmerced.dev>
2021-02-21 22:29:54 -0500

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const {
   SECRET = "secret",
   NODE_ENV = "development",
 } = process.env;
-console.log(PORT);
+
 
 //CORS
 const cors = require("cors");
@@ -24,6 +24,7 @@ const app = express();
 
 //OTHER IMPORTS
 const morgan = require("morgan");
+const {log} = require("mercedlogger")
 
 ////////////
 //MIDDLEWARE
@@ -52,5 +53,5 @@ app.get("/testjwt", (req, res) => {
 
 //LISTENER
 app.listen(PORT, () => {
-  console.log(`Your are listening on port ${PORT}`);
+  log.green("Server Start", `Your are listening on port ${PORT}`);
 });
